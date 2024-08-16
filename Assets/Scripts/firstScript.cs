1fe7169c1a6d2ddcd9c6183dfcba9953efdc2ac4
가현 <bufar1014@gmail.com>
2024-08-17 08:18:05 +0900

    변수, 연산자, 메서드, 접근자
    변수와 연산자, 메서드와 접근자 정리

--- Assets/Scripts/firstScript.cs
+++ Assets/Scripts/firstScript.cs
@@ -1,18 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class firstScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private int a;
+    public int b;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 }
