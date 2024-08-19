e34a6c5becf6969333aca2c5d312cbe2a6e4c0e0
가현 <bufar1014@gmail.com>
2024-08-19 19:22:43 +0900

    배열

--- Assets/Scripts/firstScript.cs
+++ Assets/Scripts/firstScript.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,27 +7,6 @@ using UnityEngine;
 public class FirstScript : MonoBehaviour
 
 {
-    public GameObject a;
-    public Transform b;
-
-    public Vector3 b_position;
-    public Vector3 b_rotation;
-    public Vector3 b_scale;
-
-    private void Start()
-    {
-   
-        a.GetComponent<SpriteRenderer>().color = Color.yellow;
-
-        b.position = b_position;
-        b.eulerAngles = b_rotation;
-        b.localScale = b_scale;
-
-        a.transform.position = b_position;
-        b.gameObject.SetActive(true);
-    }
-
-    
-
-
+    public int[] a;
+     
 }
