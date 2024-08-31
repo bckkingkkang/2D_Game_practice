b7cf5d204ccd3397e591a0bc11c5a5e4808c6756
가현 <bufar1014@gmail.com>
2024-08-31 17:19:38 +0900

    translate를 이용한 캐릭터 이동

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,7 +12,24 @@ public class Player : MonoBehaviour
     */
 
     // 속도
-    public float speed;
+    private float speed = 3;
+
+    // Input Key
+
+    /*
+        Input.GetKeyDown : 키가 한 번 눌렸을 경우
+        Input.GetKey() : 키가 눌려있는 동안
+        Input.GetKeyUp : 키가 떼어진 경우
+
+        Input.MouseButtonDown : 마우스가 한 번 눌렸을 경우
+        Input.MouseButton : 마우스가 눌려있는 동안
+        Input.MouseButtonUp : 마우스가 눌렸다가 떼어진 경우
+    */
+
+    // Translate(x, y, z) : Object를 x, y, z 만큼 이동시킨다.
+    // Time.deltaTime : 1초를 프레임으로 나눈 값
+    // 모든 컴퓨터 프레임에서 동일한 설정 값으로 동작하기 위해서 
+
 
     void Start()
     {
@@ -23,6 +40,18 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        transform.position += new Vector3(speed, 0.0f, 0.0f);
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            /*transform.position += new Vector3(speed, 0.0f, 0.0f);*/
+
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+
+        } else if (Input.GetKey(KeyCode.LeftArrow)) {
+
+            /*transform.position += new Vector3(-speed, 0.0f, 0.0f);*/
+
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+
     }
 }
